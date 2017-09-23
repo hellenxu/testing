b7719a7c65fe6d96f0e2994bdba8b3d079ae6909
hellenxu <heavensxu@gmail.com>
2017-09-23 11:10:15 -0400

    Add: test suite.

---
+++ UnitTestApp/app/src/test/java/ca/six/unittestapp/todo/TodoUnitTestSuite.java
@@ -0,0 +1,21 @@
+package ca.six.unittestapp.todo;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import ca.six.unittestapp.todo.data.source.TasksRepositoryTest;
+import ca.six.unittestapp.todo.statistics.StatisticsPresenterTest;
+import ca.six.unittestapp.todo.taskdetail.TaskDetailPresenterTest;
+import ca.six.unittestapp.todo.tasks.TasksPresenterTest;
+
+/**
+ * @author hellenxu
+ * @date 2017-09-23
+ * Copyright 2017 Six. All rights reserved.
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TasksRepositoryTest.class, StatisticsPresenterTest.class,
+        TaskDetailPresenterTest.class, TasksPresenterTest.class})
+public class TodoUnitTestSuite {
+}
